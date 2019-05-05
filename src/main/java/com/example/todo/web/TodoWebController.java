9d1948d4c1dec10db038361fbe1826fd6b0e8d3d
Kitamura <kitamura@plumsa.co.jp>
2019-05-05 17:15:23 +0900

    最初のコミット

---
+++ src/main/java/com/example/todo/web/TodoWebController.java
@@ -0,0 +1,125 @@
+package com.example.todo.web;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.todo.domain.Task;
+import com.example.todo.form.TaskForm;
+import com.example.todo.service.TodoService;
+
+@Controller
+public class TodoWebController {
+	public static final String TASKS = "tasks";
+	private static final String REDIRECT_TO = "redirect:/" + TASKS;
+	
+	@Autowired
+	TodoService todoService;
+	
+	//タスクを全件取得
+	@GetMapping(value = "/tasks")
+	public ModelAndView readAllTask() {
+		TaskForm form = createInitialForm();
+		ModelAndView modelAndView = toTasksPage();
+		modelAndView.addObject("form", form);
+		List<Task> tasks = todoService.findAllTasks();
+		modelAndView.addObject(TASKS, tasks);
+		return modelAndView;
+	}
+	
+	private ModelAndView toTasksPage() {
+		return new ModelAndView(TASKS);
+	}
+	
+	private TaskForm createInitialForm() {
+		String formSubject = "";
+		LocalDate formDeadLine = LocalDate.now();
+		Boolean isNewTask = true;
+		Boolean hasDone = false;
+		return new TaskForm(formSubject, formDeadLine, hasDone, isNewTask);
+	}
+	
+	// タスクを更新する
+	@PutMapping(value = "/tasks/{id}")
+	public ModelAndView updateOneTask(@PathVariable Integer id,
+			@ModelAttribute TaskForm form) {
+		updateTask(id, form);
+		return new ModelAndView(REDIRECT_TO);
+	}
+	
+	// タスクを作成する
+	@PostMapping(value = "/tasks")
+	public ModelAndView createOneTask(@ModelAttribute TaskForm form) {
+		createTaskFromForm(form);
+		return new ModelAndView(REDIRECT_TO);
+	}
+
+	// タスクを1件取得する
+	@GetMapping(value = "/tasks/{id}")
+	public ModelAndView readOneTask(@PathVariable Integer id) {
+		Optional<TaskForm> form = readTaskFromId(id);
+		if (!form.isPresent()) { 
+			return new ModelAndView(REDIRECT_TO);
+		}
+		ModelAndView modelAndView = toTasksPage();
+		modelAndView.addObject("taskId", id);
+		modelAndView.addObject("form", form.get());
+		List<Task> tasks = todoService.findAllTasks();
+		modelAndView.addObject(TASKS, tasks);
+		return modelAndView;
+	}
+	
+	private void createTaskFromForm(TaskForm form) {
+		String subject = form.getSubject();
+		LocalDate deadLine = form.getDeadLine();
+		Boolean hasDone = form.getHasDone();
+		Task task = new Task(subject, deadLine, hasDone);
+		todoService.createTask(task);
+	}
+
+	private Optional<TaskForm> readTaskFromId(Integer id) {
+		Optional<Task> task = todoService.findOneTask(id);
+		if (!task.isPresent()) {
+			return Optional.ofNullable(null);
+		}
+		String formSubject = task.get().getSubject();
+		LocalDate formDeadLine = task.get().getDeadLine();
+		Boolean hasDone = task.get().getHasDone();
+		Boolean isNewTask = false;
+		TaskForm form = new TaskForm(formSubject, formDeadLine, hasDone, isNewTask);
+		return Optional.ofNullable(form);
+	}
+	
+	private void updateTask(Integer id, TaskForm form) {
+		String subject = form.getSubject();
+		LocalDate deadLine = form.getDeadLine();
+		Boolean hasDone = form.getHasDone();
+		Task task = new Task(id, subject, deadLine, hasDone);
+		todoService.updateTask(task);
+	}
+
+	// タスクを削除する
+	@DeleteMapping(value = "/tasks/{id}") 
+	public ModelAndView deleteOneTask(@PathVariable Integer id) {
+		deleteTask(id);
+		return new ModelAndView(REDIRECT_TO);
+	}
+
+	private void deleteTask(Integer id) {
+		Optional<Task> task = todoService.findOneTask(id);
+		if (task.isPresent()) {
+			todoService.deleteTask(id);
+		}
+	}
+	
+}
